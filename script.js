894b122a3c452287d79c9004980bdf15505ea489
Yousuke Nakano <yousuke.computer@gmail.com>
2021-01-11 12:39:42 +0000

    requestFullscreen のテスト

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 (function() {
   'use strict';
 
+  const button = document.getElementById('button');
+
+  console.log('foo');
+  button.onclick = () => {
+    console.log('pushed button')
+    document.body.requestFullscreen();
+  }
 })();
