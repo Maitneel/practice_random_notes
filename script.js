3081d9ae473d8dccba14d71d1d2cec52ff7249ef
Yousuke Nakano <yousuke.computer@gmail.com>
2021-01-11 12:53:39 +0000

    clientWidth, clientHeight の確認

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
   const button = document.getElementById('button');
 
   console.log('foo');
+  document.getElementById('client').innerText = 'clientwidth  = ' + document.documentElement.clientWidth + '\n' + 'clientheight = ' + document.documentElement.clientHeight
   button.onclick = () => {
     console.log('pushed button')
     document.body.requestFullscreen();
